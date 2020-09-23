325989f22abf7d18d843d780870a3eef5efd7535
Justin Yao Du <justinyaodu@gmail.com>
2020-09-22 22:29:58 -0700

    Group events with the same date

--- js/render.js
+++ js/render.js
@@ -33,11 +33,59 @@ function renderTrackSummary(track) {
  */
 function renderEventGroups(track) {
   // TODO Reimplement this function after implementing date parsing.
-  const eventElements = [];
+  const groupedByDate = [];
+
   for (const schEvent of track.events) {
-    eventElements.push(renderEvent(schEvent));
+    if (groupedByDate.length == 0) {
+      groupedByDate.push([]);
+    } else {
+      const lastEvent = groupedByDate[groupedByDate.length - 1][0];
+
+      // Compare dates by their millisecond time value.
+      const prevTime = (lastEvent.startDate ? lastEvent.startDate.getTime() : 0);
+      const curTime = (schEvent.startDate ? schEvent.startDate.getTime() : 0);
+
+      if (prevTime !== curTime) {
+        groupedByDate.push([]);
+      }
+    }
+
+    groupedByDate[groupedByDate.length - 1].push(schEvent);
   }
-  return eventElements;
+
+  const groupElements = [];
+  for (const group of groupedByDate) {
+    groupElements.push(renderEventGroup(group));
+  }
+  return groupElements;
+}
+
+const eventGroupDateOptions = {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+};
+
+/**
+ * Return an element representing a group of events on the same date.
+ */
+function renderEventGroup(schEvents) {
+  const div = document.createElement("div");
+  div.classList.add("group");
+
+  if (schEvents[0].startDate !== undefined) {
+    const heading = document.createElement("h3");
+    heading.innerText = schEvents[0].startDate
+      .toLocaleDateString(undefined, eventGroupDateOptions);
+    div.appendChild(heading);
+  }
+
+  for (const schEvent of schEvents) {
+    div.appendChild(renderEvent(schEvent));
+  }
+
+  return div;
 }
 
 /**
